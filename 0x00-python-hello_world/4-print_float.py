a9f8c83239654cab2097b3c9f1c9ef9f02248ffb
Tiolulope <juliusadedeji@gmail.com>
2023-11-30 02:54:15 -0800

    i can think of a string

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 number = 3.14159
-pop_up = f"{number:.2f}"
+pop_up = f"number: ${number: .2f}"
 print(pop_up)
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+give_me_back = 3
+print(str * give_me_back)
+print(str[:9])
